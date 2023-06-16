f35de55558bd59550e17ec1f3e8d93710941229c
DHAANIBAHL <135830912+DHAANIBAHL@users.noreply.github.com>
2023-06-16 12:43:59 +0530

    Add files via upload

---
+++ housie2_jun16.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 17 06:45:32 2023
+
+@author: HP
+"""
+
+import random
+
+def tambola_ticket():
+    tkt = []
+    
+    sample_space = random.sample(range(1, 90), 15)
+
+    for i in range(3):
+        ss = sample_space[i*5:(i*5)+5]
+        ss.sort()
+        tkt.append(ss)
+
+    print(tkt)
+
+ticket = tambola_ticket()
